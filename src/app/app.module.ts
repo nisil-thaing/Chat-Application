953056bf64d266bc1b901e286dd59d555367ae6f
Thai (Isaac) Nguyen <thai.nguyentran@pycogroup.com>
2018-01-06 19:17:38 +0700

    Add Messages page's dummy template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { ChatRoomsComponent } from './pages/messages/chat-rooms/chat-rooms.component';
+import { ConversationComponent } from './pages/messages/conversation/conversation.component';
 
+export const appRoutes = [
+  {
+    path: 'messages',
+    component: MessagesComponent,
+    data: {
+      title: 'Messages'
+    },
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/messages',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    MessagesComponent,
+    ChatRoomsComponent,
+    ConversationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/messages/chat-rooms/chat-rooms.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-rooms',
+  templateUrl: './chat-rooms.component.html',
+  styleUrls: ['./chat-rooms.component.scss']
+})
+export class ChatRoomsComponent { }
